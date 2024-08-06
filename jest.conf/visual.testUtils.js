8c1cfdb02fdd96f9ab080a338b7a3d89a477b436
Kshitij Thareja <kshitijthareja03@gmail.com>
2024-08-06 20:26:07 +0530

    Add option to specify custom dimensions for snapshots

--- jest.conf/visual.testUtils.js
+++ jest.conf/visual.testUtils.js
@@ -40,8 +40,8 @@ export async function renderComponent(component, props) {
   global.expect(isComponentRendered).toBe(true);
 }
 
-export async function takeSnapshot(name) {
+export async function takeSnapshot(name, options = {}) {
   if (process.env.TEST_TYPE == 'visual') {
-    await percySnapshot(page, name);
+    await percySnapshot(page, name, options);
   }
 }
