f0e1a39371fc33ff4f32cbdee8d6387a36056c7a
Yeshwanth munagapati <m.yeshwanth235@gmail.com>
2025-05-13 22:06:29 +0530

    issue-1010

--- jest.conf/visual.load-test-components.js
+++ jest.conf/visual.load-test-components.js
@@ -3,7 +3,10 @@ import Vue from 'vue';
 import KButtonWithDropdownTest from '~~/lib/buttons-and-links/__tests__/components/KButtonWithDropdownTest.vue';
 import KDropdownMenuTest from '~~/lib/KDropdownMenu/__tests__/components/KDropdownMenuTest.vue';
 import KCheckboxSlotTest from '~~/lib/KCheckbox/__tests__/components/KCheckboxSlotTest.vue';
+import KLogoTest from '~~/lib/KLogo/__tests__/components/KLogoTest.vue';
+
 
 Vue.component('KCheckboxSlotTest', KCheckboxSlotTest);
 Vue.component('KButtonWithDropdownTest', KButtonWithDropdownTest);
 Vue.component('KDropdownMenuTest', KDropdownMenuTest);
+Vue.component('KLogoTest', KLogoTest);
---
+++ lib/KLogo/__tests__/components/KLogoTest.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>
+  <KLogo
+   altText="Kolibri logo"
+   :size="150"
+  />
+ </div>
+</template>
+
+<script>
+  export default {
+    name: 'KLogoTest',
+  };
+</script>
