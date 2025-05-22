ebd0ac3a82e68eba164791b23e66813cc659bac4
Pandaa007 <194596287+Pandaa007@users.noreply.github.com>
2025-05-22 10:58:48 +0530

    address review comments

--- docs/pages/playground/index.vue
+++ docs/pages/playground/index.vue
@@ -23,6 +23,11 @@
     /> -->
 
     <!-- Play around with your component here: -->
+    <KTable
+      :headers="headers"
+      :rows="rows"
+      caption="Non Sortable Table"
+    />
   </div>
 
 </template>
@@ -38,7 +43,21 @@
   export default {
     name: 'Playground',
     data() {
-      return {};
+      return {
+        headers: [
+          { label: 'Name', dataType: 'string', columnId: 'name' },
+          { label: 'Age', dataType: 'number', columnId: 'age' },
+          { label: 'City', dataType: 'string', columnId: 'city' },
+        ],
+        rows: [
+          ['John Doe', 28, 'New York'],
+          ['Jane Smith', 34, 'Los Angeles'],
+          ['Samuel Green', 22, 'Chicago'],
+          ['Alice Johnson', 30, 'Houston'],
+          ['Michael Brown', 45, 'Phoenix'],
+          ['Emily Davis', 27, 'Philadelphia'],
+        ],
+      };
     },
     methods: {},
   };
