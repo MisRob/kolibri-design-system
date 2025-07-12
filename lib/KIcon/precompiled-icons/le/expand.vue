5f7d681cf415b1472bf6bf9bbe8c5f9da2e64d9d
WinnyChang <winnypooh123@gmail.com>
2025-07-11 17:30:17 -0700

    Add expand icon

---
+++ lib/KIcon/precompiled-icons/le/expand.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.778 8.969l1.253 1.253-4 4h3.191V16H0V9.778h1.778v3.19l4-4zM7.03 5.778L5.778 7.03l-4-4v3.191H0V0h6.222v1.778h-3.19l4 4zm3.191 3.19l4 4v-3.19H16V16H9.778v-1.778h3.19l-4-4 1.254-1.253zM8.97 5.779l4-4H9.778V0H16v6.222h-1.778v-3.19l-4 4L8.97 5.777z" fill="#000"/></svg>
+
+</template>
+
+
+<script>
+
+  export default {"name":"icon-3d5c6ba491e3cb5ed7046671b4ca1a2e"}
+
+</script>
