6762901ed9595f95e0aad2e8dbcc44177056f584
WinnyChang <winnypooh123@gmail.com>
2025-07-14 14:26:48 -0700

    Update expand icon

--- lib/KIcon/precompiled-icons/le/expand.vue
+++ lib/KIcon/precompiled-icons/le/expand.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.778 8.969l1.253 1.253-4 4h3.191V16H0V9.778h1.778v3.19l4-4zM7.03 5.778L5.778 7.03l-4-4v3.191H0V0h6.222v1.778h-3.19l4 4zm3.191 3.19l4 4v-3.19H16V16H9.778v-1.778h3.19l-4-4 1.254-1.253zM8.97 5.779l4-4H9.778V0H16v6.222h-1.778v-3.19l-4 4L8.97 5.777z" fill="#000"/></svg>
+  <svg role="presentation" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9.5 13.09l1.41 1.41-4.5 4.5H10v2H3v-7h2v3.59l4.5-4.5m1.41-3.59L9.5 10.91 5 6.41V10H3V3h7v2H6.41l4.5 4.5m3.59 3.59l4.5 4.5V14h2v7h-7v-2h3.59l-4.5-4.5 1.41-1.41M13.09 9.5l4.5-4.5H14V3h7v7h-2V6.41l-4.5 4.5-1.41-1.41z"/></svg>
 
 </template>
 
 
 <script>
 
-  export default {"name":"icon-3d5c6ba491e3cb5ed7046671b4ca1a2e"}
+  export default {"name":"icon-1f4d6379ab56119cca336e5c95ad009a"}
 
 </script>
