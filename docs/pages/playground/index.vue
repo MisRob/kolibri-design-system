d8a5bc7a79ae807d7f2f22b6591c8da33b9fe207
Michaela Robosova <robosovam@gmail.com>
2025-09-02 12:17:20 +0200

    Clean up playground

--- docs/pages/playground/index.vue
+++ docs/pages/playground/index.vue
@@ -9,15 +9,26 @@
     Please do not commit your local updates of this file.
   -->
   <div style="padding: 24px">
-    <CardsVisualTest />
+    <!-- 
+      Example: Uncomment lines bellow, change something
+      in lib/KBreadcrumbs.vue and see the updates reflected
+      on this page
+    -->
+    <!-- <KBreadcrumbs
+      :items="[
+        { text: 'Global Digital Library', link: { path: '#' } },
+        { text: 'English', link: { path: '#' } },
+        { text: 'Level 2 ', link: { path: '#' } },
+      ]"
+    /> -->
+
+    <!-- Play around with your component here: -->
   </div>
 
 </template>
 
 
 <script>
-
-  import CardsVisualTest from '../../../lib/cards/__tests__/components/CardsVisualTest.vue';
 
   /*
     Playground is a Vue component too,
@@ -26,9 +37,6 @@
   */
   export default {
     name: 'Playground',
-    components: {
-      CardsVisualTest,
-    },
     data() {
       return {};
     },
