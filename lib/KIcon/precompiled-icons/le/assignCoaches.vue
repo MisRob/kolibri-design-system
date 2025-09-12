97c5d83b6ce9530edf3e9ebb4b48821b96dc1b76
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2025-09-12 18:33:02 +0300

    update assign coach icon

--- lib/KIcon/precompiled-icons/le/assignCoaches.vue
+++ lib/KIcon/precompiled-icons/le/assignCoaches.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="23" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M12.675 7.5l-3.52 3.52h9.115v1.96H9.154l3.52 3.52-1.384 1.385L5.405 12l5.885-5.885L12.675 7.5z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M11.79 2.02c5.512 0 9.98 4.469 9.98 9.98 0 5.512-4.468 9.98-9.98 9.98-5.511 0-9.98-4.468-9.98-9.98 0-5.511 4.469-9.98 9.98-9.98zm0 1.96a8.02 8.02 0 100 16.04 8.02 8.02 0 000-16.04z"/></svg>
+  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="21" height="21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.15 10.9a9 9 0 00-8.965 8.206 1 1 0 101.992.175 7 7 0 018.2-6.274 1 1 0 10.347-1.97A9.051 9.051 0 009.15 10.9zm6.429 1.121a1 1 0 00-1.414-.043l-3.45 3.243a1 1 0 000 1.458l3.45 3.243a1 1 0 001.37-1.457l-1.611-1.515H19.2a1 1 0 100-2h-5.276l1.611-1.515a1 1 0 00.044-1.414zM6.15 5a3 3 0 116 0 3 3 0 01-6 0zm-2 0a5 5 0 1010 0 5 5 0 00-10 0z" fill="#000"/></svg>
 
 </template>
 
 
 <script>
 
-  export default {"name":"icon-44e742732714ed006be367fb9a083121"}
+  export default {"name":"icon-832c9073e063e56a1fea85c1d7903bab"}
 
 </script>
